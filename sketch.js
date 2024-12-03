80bbeacbbad6ab2fa48e706fc47aaf86654a1c37
RowanWA <Rowan.Armitage@student.norwichuni.ac.uk>
2024-12-03 15:40:17 +0000

    fsf

--- sketch.js
+++ sketch.js
@@ -1,8 +1,34 @@
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
-  
+    createCanvas(innerWidth, innerHeight);
+    noLoop();
+    rectMode(CENTER);
+}
+
+//   function draw() {
+//     background(0);
+//     fill(0, 185, 160)
+//     stroke(0, 185, 160)
+//     circle(400, 400, 150)
+//     circle(400, 300, 150)
+//     rect(325, 300, 150, 100)
+//     triangle(400, 375, 475, 650, 325, 650)
+//   }
+
+function draw() {
+    background(0, 25, 15);
+    noFill()
+    stroke(0, 185, 160, 100)
+    // rect(500, 500, 200, 200)
+    // let c = map(mouseY, 0, height, 0, 255)
+    // fill(0, c, 125)
+    fill(0, 185, 160, 100)
+    // let w = map(mouseX, 0, width, 0, 500)
+    // rect(500, 500, w, w)
+    const size = 20;
+    for(let i = 0; i < 20000; i++){
+        let x = random (size/2, width - size/2);
+        let y = random (size/2, height - size/2);
+        square(x, y, size);
+    }
+}
+
